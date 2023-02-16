042a1c90e27c0409e68382c81e7e8efade032103
Rubie-Eurickah <eurickahmatata345@gmail.com>
2023-02-16 11:20:55 -0800

    Write a C program that prints a given line.

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a given line using the puts function
+ * Return: 0(The proram run sucessfully)
+ */
+
+int main(void)
+{
+char Mystr[] = "\"Programming is like building a multilingual puzzle";
+puts(Mystr);
+return (0);
