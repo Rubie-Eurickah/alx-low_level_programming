0e6cc565d981ff6da43d9c1ff92834c307de04f5
Rubie-Eurickah <eurickahmatata345@gmail.com>
2023-02-17 12:57:06 -0800

    Prints all single digit numbers of base 10 starting from 0, followed by a new line.

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Prints all single base 10 digits from 0
+ * Return: Alwsys (Success)
+ */
+
+int main(void)
+{
+
+	int j;
+
+	for (j = 0; j < 10; j++)
+	printf("%d", j);
+	printf("\n");
+	return (0);
+}
